d7d576b78afe04d96c8db8bc64132eefac7aaf9b
Daphne Bosch <daphnebosch@hotmail.com>
2020-08-25 16:23:12 +0200

    Initial commit
    Did some changes

---
+++ mccforum/resources/views/front/components/topbar/topbar.blade.php
@@ -0,0 +1,113 @@
+<div class="page-wrapper chiller-theme toggled">
+    <div class="mccity" style="background-image: url('{{asset('/img/wallpapers/mcc2.jpg')}}'); background-repeat: no-repeat">
+
+    </div>
+    <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+        <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar" style=" background-image: url('{{asset('/img/wallpapers/nicey.jpg')}}'); background-repeat: no-repeat" class="sidebar-wrapper">
+        <div class="sidebar-content">
+            <div class="sidebar-brand">
+                <a href="#">MCC Forum</a>
+                <div id="close-sidebar">
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>
+            <div class="sidebar-header">
+                <div class="user-pic">
+                    <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
+                         alt="User picture">
+                </div>
+                <div class="user-info">
+          <span class="user-name"><strong>Shayley</strong>
+          </span>
+                    <span class="user-role">Owner</span>
+                    <span class="user-status">
+            <i class="fa fa-circle"></i>
+            <span>Online</span>
+          </span>
+                </div>
+            </div>
+            <!-- sidebar-header  -->
+            <div class="sidebar-search">
+                <div>
+                    <div class="input-group">
+                        <input type="text" class="form-control search-menu" placeholder="Search...">
+                        <div class="input-group-append">
+              <span class="input-group-text">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- sidebar-search  -->
+            <div class="sidebar-menu">
+                <ul>
+                    <li class="header-menu">
+                        <span>Forum</span>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="/">
+                            <i class="fas fa-home"></i>
+                            <span>Home</span>
+                            <span class="badge badge-pill badge-warning">Being worked on</span>
+                        </a>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="fas fa-user"></i>
+                            <span>Members</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="far fa-newspaper"></i>
+                            <span>News</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="fa fa-chart-line"></i>
+                            <span>Vote</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="far fa-question-circle"></i>
+                            <span>Help</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    </li>
+                    <li class="header-menu">
+                        <span>More</span>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-shopping-cart"></i>
+                            <span>Store</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-calendar"></i>
+                            <span>Events</span>
+                            <span class="badge badge-pill badge-primary">W.I.P</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-folder"></i>
+                            <span>Examples</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- sidebar-menu  -->
+        </div>
+    </nav>
+</div>
+
+<script src="{{asset('js/slide.js')}}"></script>
---
+++ mccforum/resources/views/front/home/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.layout')
+
+@section('title', 'MCCForum | Home')
+
+@section('content')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8">
+                <div class="homecontent">
+                    <div class="card bg-light mb-3 justify-content-center">
+                        <div class="card-header"><h4>Hey, nice to see you!</h4></div>
+                        <div class="card-body">
+                            <p class="card-text">Welcome and enjoy :D</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card bg-light mb-3 justify-content-center">
+                    <div class="card-header"><h4>Hey, this is a work in progress</h4></div>
+                    <div class="card-body">
+                        <p class="card-text">I'm working on the website and hope to finish it as soon as possible </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
